3e1e76efc7bf1e41a51a5c7df30d95823f3f75a3
Michael Emhofer <emi@fsinf.at>
2012-12-15 19:02:32 +0100

    started game

--- game.rb
+++ game.rb
@@ -2,10 +2,11 @@ require '../blinkofant-ruby/screen'
 require '../blinkofant-ruby/device'
 
 module BlinkofantTunnel
-	scr = Blinkofant::Screen.new(panels = 1)
+	scr = Blinkofant::Screen.new(1)
 	dev = Blinkofant::Device.new
 
 	while(true)
+		puts 
 		scr[1,1] = true
 		scr[5,5] = true
 		dev.flush(scr)
